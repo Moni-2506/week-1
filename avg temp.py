0523d9921f5857fa63f7c93222a95a4bb0c16b27
Moni-2506 <seventiny17@gmail.com>
2026-02-19 18:17:16 +0530

    Add files via upload

---
+++ avg temp.py
@@ -0,0 +1,10 @@
+
+print ("Average temperature calculator")
+num=int(input("how many temperature "))
+total = 0;
+for i in range(n):
+ temperature = float(input(f"enter temparture {i+1}:"))
+total +=temperature
+average = total/num
+print ("average temperature:" ,average)
+
